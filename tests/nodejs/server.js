a7bf72636bbcbb68c310812f90b582c24a2d0d82
puritys <pipi00000@gmail.com>
2015-02-16 13:20:10 +0800

    add unit test

---
+++ tests/nodejs/server.js
@@ -0,0 +1,41 @@
+var assert = require("assert");
+var serverObj = require("./../../core/nodejs/server");
+var xml = require('libxmljs');
+var root = __dirname + '/../../examples/';
+var tester = new serverObj(root);
+
+var cssResult = ""
+var baseRes = {
+    write: function(text) {
+        cssResult = text;
+    },
+    end: function() {},
+    contentType: function() {}
+};
+var baseReq = {
+};
+
+describe("Test: Compile less", function () {
+    it("simple less to css", function () {
+        var req = baseReq;
+        var expect = ".class {\n" +
+                     "  width: \"2px\";\n" + 
+                     "}\n";
+        req["baseUrl"] = "/static/css/simple.less";
+
+        var result = tester.loadLess(req, baseRes);
+        assert.equal(expect, cssResult);
+    });
+
+    it("file not found", function () {
+        var req = baseReq;
+        var expect = ".fileNotFound{}";
+        req["baseUrl"] = "/xxxx.less";
+
+        var result = tester.loadLess(req, baseRes);
+        assert.equal(expect, cssResult);
+    });
+
+});
+
+
